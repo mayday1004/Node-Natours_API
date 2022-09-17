77aa9d2c29744ca16e929a505a6cab10e3b492c9
mayday1004 <iko7695@gmail.com>
2022-09-17 17:48:05 +0800

    連接mongodb altas

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './.env' });
 const app = require('./app');
+
+mongoose
+  .connect(process.env.DB)
+  .then(() => console.log('DB connection successful! 😎'))
+  .catch(() => console.log('DB connection failed! 😭'));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
