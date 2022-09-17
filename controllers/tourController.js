fe0b9918c714b3748c95412fc6e8ec689885e192
mayday1004 <iko7695@gmail.com>
2022-09-17 23:42:48 +0800

    添加最便宜的5個旅遊行程路徑

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,12 @@
 const Tour = require('../models/tourModel');
 
+exports.topFiveCheap = async (req, res, next) => {
+  req.query.limit = 5;
+  req.query.sort = '-ratingAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     // Build query
--- routes/tourRouter.js
+++ routes/tourRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const tourController = require('../controllers/tourController');
 
+router.route('/top-5-cheap').get(tourController.topFiveCheap, tourController.getAllTours);
 router.route('/').get(tourController.getAllTours).post(tourController.createTour);
 router
   .route('/:id')
